1ddec255e233722141c2301518c34f027631288f
empty <empty>
2024-05-04 22:17:16 +0300

    Step 3

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import * as rs from 'readline-sync';
+const greetings=()=>{
+    console.log('Welcome to the Brain Games!');
+    const name=rs.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+};
+export default greetings;
