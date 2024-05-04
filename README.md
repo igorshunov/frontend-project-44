8a4edf0efe929e9baa0d56adfe528360753effed
empty <empty>
2024-05-04 22:50:49 +0300

    Step 4

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/igorshunov/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/igorshunov/frontend-project-44/actions)
+
+<a href="https://codeclimate.com/github/igorshunov/frontend-project-44/maintainability"><img src="https://api.codeclimate.com/v1/badges/70d345e2df6cc543b4d0/maintainability" /></a>
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import * as rs from 'readline-sync';
-const greetings=()=>{
-    console.log('Welcome to the Brain Games!');
-    const name=rs.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
+
+const greetings = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = rs.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
 export default greetings;
